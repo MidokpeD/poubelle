edd0b77dea162e753d253062e630dec8cf9e34a8
MidokpeD <160834660+MidokpeD@users.noreply.github.com>
2024-02-22 09:11:41 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+let Distance = 0
+basic.forever(function () {
+    Distance = sonar.ping(
+    DigitalPin.P0,
+    DigitalPin.P1,
+    PingUnit.Centimeters
+    )
+    if (Distance < 6) {
+        basic.showIcon(IconNames.No)
+    } else {
+        basic.clearScreen()
+    }
+})
